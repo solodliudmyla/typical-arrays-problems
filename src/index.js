f2406b4567dcdb13c82128e830d2a6c46e691075
Solod Liudmyla <solodludmila@gmail.com>
2020-03-20 15:54:15 +0200

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if (array === undefined || array.length === 0) return 0;
+    else return   Math.min(array);
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+    if (array === undefined || array.length === 0) return 0;
+    else return   Math.max(array);
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    if (array === undefined || array.length === 0) return 0;
+    else return array.reduce( (sum, i) => sum + i ) / array.length;
+};
